33eb965e42300eb8fdf2276a2e0d41146f832e26
Mohammad Mahdi Jahanara <JahanaraCoYazd@gmail.com>
2015-02-09 00:45:35 +0330

    Create Dijkstra.cpp

---
+++ Dijkstra.cpp
@@ -0,0 +1,54 @@
+/*
+	In the name of Allah
+	Code by jahanaraco
+	Single Source Shortest Path
+	Dijkstras algorithm for Weighted Graphs
+	O(E lg N)
+*/
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+const int MAXN = 1000;
+int n, m, source, dist[MAXN];
+vector<pair<int, int> > adj[MAXN];
+set<pair<int, int> > S; // Set of candidates vertices
+
+int main(){
+	cin >> n >> m >> source; 
+	for(int i = 0; i < m; i++){
+		int v, u, w;
+		cin >> v >> u >> w;
+		adj[v].push_back(make_pair(u, w));
+		adj[u].push_back(make_pair(v, w));
+	}
+	
+	for(int i = 1; i <= n; i++)
+		dist[i] = -1; // -1 means there's no path yet
+	
+	dist[source] = 0;
+	S.insert(make_pair(0, source));
+	while(!S.empty() /*S.size() > 0 */){
+		int v = S.begin() -> second;
+		// int v = (*S.begin()).second;
+		S.erase(S.begin());
+		for(int i = 0; i < adj[v].size(); i++){
+			int u = adj[v][i].first;
+			int w = adj[v][i].second;
+			if(dist[u] == -1 or dist[u] > dist[v] + w){
+				if(dist[u] != -1) 
+					S.erase(make_pair(dist[u], u));
+				dist[u] = dist[v] + w;
+				S.insert(make_pair(dist[u], u));
+			}
+		}
+	}	
+	
+	for(int i = 1; i <= n; i++)
+		cout << dist[i] << ' ' ;
+	cout << endl;
+	return 0;
+}
